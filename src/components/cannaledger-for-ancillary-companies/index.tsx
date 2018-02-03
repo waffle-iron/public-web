b70d3729879cea4d08bc35f122c7024905d254d2
Nick <nicholas.shook@gmail.com>
2018-02-03 11:56:21 -0800

    added some basic copy & initial commit

---
+++ src/components/cannaledger-for-ancillary-companies/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+class CannaledgerForAncillaryCompanies extends React.Component {
+    render() {
+        return (
+            <div className="CannaledgerForAncillaryCompanies">
+              <h1>Cannaledger for Ancillary Companies</h1>
+
+              Compliance doesn't stop at the establishment entities. Companies
+              that support the cannabis industry ought to be transparent about
+              their data as well. For instance, a nutrient company should disclose
+              their ingredients and storage conditions to best ensure patient
+              safety with a safe product.
+            </div>
+        );
+    }
+}
+
+export default CannaledgerForAncillaryCompanies;
---
+++ src/components/participating-ancillary-companies/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class ParticipatingAncillaryCompanies extends React.Component {
+    render() {
+        return (
+            <div className="CannaledgerForEstablishedLicenses">
+              <h1>Grow Generation</h1>
+            </div>
+        );
+    }
+}
+
+export default ParticipatingAncillaryCompanies;
