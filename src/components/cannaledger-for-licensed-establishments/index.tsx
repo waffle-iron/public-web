dccf969fa69838d693911ec2eacd676393a735b6
Nick <nicholas.shook@gmail.com>
2018-02-06 07:29:22 -0800

    blah

--- src/components/cannaledger-for-licensed-establishments/index.tsx
+++ src/components/cannaledger-for-licensed-establishments/index.tsx
@@ -7,7 +7,8 @@ class CannaledgerForEstablishedLicenses extends React.Component {
               <h1>Cannaledger for Licensed Establishements</h1>
 
               Establishments that grow, produce, or sell marijuana in America
-              must be registered with the state of business.
+              must be registered with the state of business, and must report
+              certain information like 
 
               Cannaledger interfaces with Metrc, the required reporting tool for
               many American states including Nevada, California, Oregon, and
