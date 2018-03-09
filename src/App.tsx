6865c53295d3ac997007ec34674262446b6ab8a8
Nick <nicholas.shook@gmail.com>
2018-03-09 12:56:53 -0800

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import './App.css';
 
-import CannaledgerForAncillaryCompanies from './components/cannaledger-for-ancillary-companies';
-import CannaledgerForLicensedEstablishements from './components/cannaledger-for-licensed-establishments';
-import ParticipatingAncillaryCompanies from './components/participating-ancillary-companies';
-import ParticipatingLicensedEntities from './components/participating-licensed-entities';
+import Info from './routes/info';
 import Portal from './routes/portal';
 
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -22,7 +19,6 @@ import { Link, Route } from 'react-router-dom';
 const store = createStore(AppReducer);
 const uri = 'http://localhost:4200/graphql';
 
-const logo = require('./logo.svg');
 const client = new ApolloClient({
   link: new HttpLink({ uri }),
   cache: new InMemoryCache(),
@@ -35,52 +31,10 @@ const App = () => {
         <BrowserRouter>
           <div className="App">
             <Link to="/portal">Log into the portal</Link>
+            <Link to="/info">Get Info</Link>
 
+            <Route path="/info" component={Info} />
             <Route path="/portal" component={Portal} />
-
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Cannaledger</h1>
-            </header>
-            <h2 className="App-title">
-              transparent self-governance for the worldwide cannabis industry
-            </h2>
-            <p>For Companies</p>
-            <p>For Cannabis Patients & Consumers</p>
-            <p className="App-intro">
-              The cannaledger is an open, distributed blockchain for recording
-              actions undertaken by workers in the cannabis industry.
-            </p>
-            <h1>So what does any of this mean?</h1>
-            <p>
-              Typically, digital data is hosted on a company's server, this
-              means that all the data owned by a company exclusively belongs to
-              them. The blockchain is different in that instead of data being
-              hosted in one spot, it is hosted in as many places as possible,
-              with the blockchain itself taking care of data duplication and
-              redudancy.
-            </p>
-            <p>
-              While all the data is recorded publically, each data point can be
-              protected in terms of who gets to access it by proven
-              cryptographic mathematics.
-            </p>
-            <p className="App-intro">
-              It is built on top of Hyperledger Sawtooth, and uses state of the
-              art consensus algorithms to ensure data integrity and speed as
-              actions get recorded to the ledger.
-            </p>
-            <h1>Interfacing with Cannaledger</h1>
-            <p className="App-intro">
-              If you're technically inclined, we encourage you to develop your
-              own interfaces to the blockchain itself. Otherwise the Cannaledger
-              Incorporation offers workplace interfaces available for $10 per
-              employee per month.
-            </p>
-            <CannaledgerForLicensedEstablishements />
-            <CannaledgerForAncillaryCompanies />
-            <ParticipatingLicensedEntities />
-            <ParticipatingAncillaryCompanies />
           </div>
         </BrowserRouter>
       </Provider>
---
+++ src/routes/info.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+
+const logo = require('../logo.svg');
+
+import CannaledgerForAncillaryCompanies from '../components/cannaledger-for-ancillary-companies';
+import CannaledgerForLicensedEstablishements from '../components/cannaledger-for-licensed-establishments';
+import ParticipatingAncillaryCompanies from '../components/participating-ancillary-companies';
+import ParticipatingLicensedEntities from '../components/participating-licensed-entities';
+
+const InfoRoute = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Cannaledger</h1>
+      </header>
+      <h2 className="App-title">
+        transparent self-governance for the worldwide cannabis industry
+      </h2>
+      <p>For Companies</p>
+      <p>For Cannabis Patients & Consumers</p>
+      <p className="App-intro">
+        The cannaledger is an open, distributed blockchain for recording actions
+        undertaken by workers in the cannabis industry.
+      </p>
+      <h1>So what does any of this mean?</h1>
+      <p>
+        Typically, digital data is hosted on a company's server, this means that
+        all the data owned by a company exclusively belongs to them. The
+        blockchain is different in that instead of data being hosted in one
+        spot, it is hosted in as many places as possible, with the blockchain
+        itself taking care of data duplication and redudancy.
+      </p>
+      <p>
+        While all the data is recorded publically, each data point can be
+        protected in terms of who gets to access it by proven cryptographic
+        mathematics.
+      </p>
+      <p className="App-intro">
+        It is built on top of Hyperledger Sawtooth, and uses state of the art
+        consensus algorithms to ensure data integrity and speed as actions get
+        recorded to the ledger.
+      </p>
+      <h1>Interfacing with Cannaledger</h1>
+      <p className="App-intro">
+        If you're technically inclined, we encourage you to develop your own
+        interfaces to the blockchain itself. Otherwise the Cannaledger
+        Incorporation offers workplace interfaces available for $10 per employee
+        per month.
+      </p>
+      <CannaledgerForLicensedEstablishements />
+      <CannaledgerForAncillaryCompanies />
+      <ParticipatingLicensedEntities />
+      <ParticipatingAncillaryCompanies />
+    </div>
+  );
+};
+
+export default InfoRoute;
